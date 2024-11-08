7761acc20c02b6a3f12658e1da38bba461e19e0f
iejr <15151370+iejr@users.noreply.github.com>
2024-11-08 22:29:54 +0800

    Remove unused comments

--- Ledger/invest_manager.py
+++ Ledger/invest_manager.py
@@ -175,30 +175,3 @@ if __name__ == "__main__":
 
     args = sys.argv[1:]
     main(args)
-    # ledger = Ledger("Token")
-
-    # ledger.addNewPurchase(100, 20)
-    # ledger.addNewSellPlan("plan 1", 101, 20)
-
-    # ledger.addNewPurchase(50, 20)
-    # ledger.addNewSellPlan("plan 2", 50, 10)
-
-    # ledger.addNewPurchase(150, 20)
-    # ledger.addNewSellPlan("plan 3", 200, 10)
-    # ledger.addNewSellPlan("plan 4", 75, 10)
-
-    # ledger.addNewPurchase(150, 20)
-    # ledger.addNewSellPlan("plan 5", 150, 20)
-
-    # ledger.finishPlan("plan 1", True)
-    # ledger.finishPlan("plan 3", False)
-
-    # ledger2 = Ledger("Token2")
-    # ledger2.addNewPurchase(5, 500)
-
-    # saveData([ledger.toDict(), ledger2.toDict()], "temp.yaml")
-    # ledger_raw = loadData("temp.yaml")
-    # print(ledger_raw)
-
-    # ledgers = [Ledger.fromDict(ledger) for ledger in ledger_raw]
-    # print(ledgers)
